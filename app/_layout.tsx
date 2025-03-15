9899e903ba1d05221e38f185c99a8c76782a64c4
Steven Satish <stevensatish12@gmail.com>
2025-03-14 20:01:04 -0500

    initialize firebase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack initialRouteName="Login">
+        <Stack.Screen name="Login" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
 }
